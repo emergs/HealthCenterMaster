fb29ddb50d83bc5332510fc794186ae13a63e247
Emerson <emerson.gsantos@luizalabs.com>
2024-01-20 15:17:47 -0300

    adjustment of the news and maps section

--- Models/Base.cs
+++ Models/Base.cs
@@ -1,10 +1,23 @@
+using System.Data;
+
 namespace HealthCenterMaster.Models;
 
-public class Time
+public class Team
 {
     public string? Image { get; set; }
     public string? Name { get; set; }
-    public string? Specialty { get; set; }
+    public string? Specialty { get; set; }  
     public string? Phone { get; set; }
     public string? Email { get; set; }
+}
+
+public class News
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public string? Specialty { get; set; }  
+    public string? Author { get; set; }
+    public string? Author_Specialty { get; set; }
+    public DateTime Data {get; set;}
+    public string? Image { get; set; }
 }
