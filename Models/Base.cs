9c8730e3989bac69c1492e437f432d8c6b2d4294
Emerson <emerson.gsantos@luizalabs.com>
2024-01-24 19:52:45 -0300

    ajuste nos dados do footer

--- Models/Base.cs
+++ Models/Base.cs
@@ -6,7 +6,7 @@ public class Team
 {
     public string? Image { get; set; }
     public string? Name { get; set; }
-    public string? Specialty { get; set; }  
+    public string? Specialty { get; set; }
     public string? Phone { get; set; }
     public string? Email { get; set; }
 }
@@ -15,9 +15,15 @@ public class News
 {
     public string? Title { get; set; }
     public string? Content { get; set; }
-    public string? Specialty { get; set; }  
+    public string? Specialty { get; set; }
     public string? Author { get; set; }
     public string? Author_Specialty { get; set; }
-    public DateTime Data {get; set;}
+    public DateTime Date { get; set; }
     public string? Image { get; set; }
+}
+
+public class Infos
+{
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
 }
